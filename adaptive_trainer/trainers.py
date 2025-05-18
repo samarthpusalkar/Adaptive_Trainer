Fix adaptive loss calculation by removing unnecessary squaring of the loss function

adaptive_trainer/trainers.py
@@ -320,7 +320,7 @@ class AdaptiveTrainer(Trainer):
         # Recalculating proper training tokens
         stats['training_mask'] = flat_learn_style_mask_attention.sum().item() + flat_learn_style_mask_ideas.sum().item()
         alpha_attention_bias = torch.tensor(1.0, requires_grad=True)
-        loss = (attention_learning_loss*alpha_attention_bias + ideas_learning_loss*(2-alpha_attention_bias))**2
+        loss = (attention_learning_loss*alpha_attention_bias + ideas_learning_loss*(2-alpha_attention_bias))
 
         return loss, stats
 